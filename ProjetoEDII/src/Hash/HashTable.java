6a00da18e1d06704c82ea8ceb014955b293da025
Gustavo Melo Silva <160752765+Gut0oo@users.noreply.github.com>
2025-11-05 01:22:06 +0000

    add

--- ProjetoEDII/src/Hash/HashTable.java
+++ ProjetoEDII/src/Hash/HashTable.java
@@ -11,11 +11,36 @@ public class HashTable{
         this.tam = tam;
     }
 
-    public void put(String palavra, int freq){
+    public void inserir(String palavra, int freq){
+        int index = metodoDivisao(palavra); //calcula o indice a ser inserido
+        Node no = new Node(palavra, freq);
 
+        tabela[index].addFirst(no); //adiciona no inicio da lista -> O(1)
     }
 
-    public void func(String dado){
+    public Node buscas(String palavra){
+        Node  no = null;
+        return no;
+    }
 
+    
+
+    //Metodo de divisão
+    public int metodoDivisao(String dado){
+        int stringTam = dado.length(); //guarda o tamanho da string
+
+        int hash = (int) dado.charAt(0);
+
+        for(int i = 0; i < stringTam; i++){
+            hash += (int) dado.charAt(i); //soma o codigo Ascii de cada codigo
+        }
+
+        hash *= hash; //Elevando a 2 para trazer uma melhor distribuição na tabela
+
+        return (hash % tam);
+    }
+
+    public int metodoX(){
+        return 0;
     }
 }
--- ProjetoEDII/src/model/Resultado.java
+++ ProjetoEDII/src/model/Resultado.java
@@ -2,7 +2,7 @@ package model;
 
 public class Resultado {
     private double similaridade;
-    private String doc1, doc2; //nome dos arquivos que serão comparados
+    private String doc1, doc2; //nome dos arquivos que foram comparados
 
     public Resultado(String doc1, String doc2){
         this.doc1 = doc1;
@@ -10,8 +10,14 @@ public class Resultado {
         this.similaridade = 0.0;
     }
 
-    //Método pro Calculo de Similaridade: 
-    public double similaridadeCalculo(){
-        return 0.0;
-    }
+
+    //getters & setters
+    public double getSimilaridade(){ return similaridade; }
+    public void setSimilaridade(double similaridade){ this.similaridade = similaridade; }
+
+    public String getDoc1(){ return doc1; }
+    public void setDoc1(String doc1){ this.doc1 = doc1; }
+
+    public String getDoc2(){ return doc2; }
+    public void setDoc2(String doc2){ this.doc2 = doc2; }
 }
