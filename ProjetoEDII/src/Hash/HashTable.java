66e5a8aad2b657adc4e7c03b5fc018dafa3aec36
Gustavo Melo Silva <160752765+Gut0oo@users.noreply.github.com>
2025-11-14 21:24:51 +0000

    hash finalizado

--- ProjetoEDII/src/Hash/HashTable.java
+++ ProjetoEDII/src/Hash/HashTable.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 
 public class HashTable{
     private LinkedList<Node>[] tabelaHash; //um array de lista ligada
-    private int contColisao;
+    private int contColisao, qtd;
     private int tam;
 
     public HashTable(int tam){
@@ -14,10 +14,16 @@ public class HashTable{
         }
     
         this.tam = tam;
-        this.contColisao = 0;
+        this.contColisao = this.qtd = 0;
     }
 
     public void put(String palavra, int freq){
+        double fatorCarga = (double)qtd / tam; //calcula o fator de carga
+
+        if(fatorCarga > 0.8){ //antes de inserir o novo elemento, verifica se precisa realizar um resize
+            resize();
+        }
+
         int index = metodoDivisao(palavra); //calcula o indice a ser inserido
 
         for(Node no : tabelaHash[index]){
@@ -32,6 +38,7 @@ public class HashTable{
         }
 
         tabelaHash[index].addFirst(new Node(palavra, freq)); //adiciona no inicio da lista -> O(1)
+        qtd++; //faz a contagem da quantidade de elementos
     }
 
     public Node get(String palavra){
@@ -50,6 +57,30 @@ public class HashTable{
         return no;
     }
 
+    public void resize(){
+        int novoTam = this.tam * 2; //dobra o tamanho
+        LinkedList<Node>[] novaTabelaHash = new LinkedList[novoTam];
+
+        for(int i = 0; i < novoTam; i++){ //inicializa cada indice com uma lista ligada - evita erro nullPointer
+            novaTabelaHash[i] = new LinkedList<>();
+        }
+
+        LinkedList<Node>[] antigaTabela = tabelaHash;//guarda a tabela anterior
+
+        //atualiza os valores da tabela modifica
+        this.tabelaHash = novaTabelaHash;
+        this.tam = novoTam;
+        this.contColisao = 0;
+
+
+        for(LinkedList<Node> lista : antigaTabela){ //aqui vai add os nós da tabela antiga na tabela nova
+            for(Node no : lista){
+                put(no.getPalavra(), no.getFreq());//aqui chama o metodo de inserção, assim inserindo no local certo
+            }
+        }
+
+    }
+
     //Metodo de divisão
     public int metodoDivisao(String dado){
         int stringTam = dado.length(); //guarda o tamanho da string
@@ -63,8 +94,20 @@ public class HashTable{
         return Math.abs(hash % tam); //evita numero negatvios caso hash estoure o limite
     }
 
-    public int metodoX(){
-        return 0;
+    public int metodoMultiplicacao(String dado){
+        double A = (Math.sqrt(5) - 1) / 2; //constante
+        
+        int stringTam = dado.length();
+        int hash = (int) dado.charAt(0);
+
+        for(int i = 1; i < stringTam; i++){
+            hash = (31 * hash) + dado.charAt(i); //31 eh primo, assim dando uma melhor dispersão 
+        }//o numero primo eh mlr pq não tem divisores alem de 1 e ele mesmo, assim evitando padrões de repetição
+
+
+        double parteFrac = (hash * A) % 1; //pega a parte fracionaria
+
+        return (int) (tam * parteFrac);
     }
 
     //Getter e Setters
